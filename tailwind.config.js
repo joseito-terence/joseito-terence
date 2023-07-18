c650037f17c370726b90936bbbadb82aa8e41251
Joseito Fernandes <joseito.terence@gmail.com>
2023-07-18 18:07:02 +0530

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cyberpunk"],
+    themes: ["light", "night", "cyberpunk"],
+    darkTheme: 'night'
   },
 };
