68354b90646d24886d503eaeedee41a6bbe50e6d
Joseito Fernandes <joseito.terence@gmail.com>
2023-07-20 22:22:30 +0530

    infinite scroll for skills

--- src/routes/Skills.svelte
+++ src/routes/Skills.svelte
@@ -8,7 +8,7 @@
 	import Git from '$lib/icons/git.svelte';
 	import Database from '$lib/icons/database.svelte';
 
-	const skills = [
+	let skills = [
 		{ name: 'React', icon: React },
 		{ name: 'Sass', icon: Sass },
 		{ name: 'Javascript', icon: Javascript },
@@ -18,16 +18,16 @@
 		{ name: 'Git', icon: Git },
 		{ name: 'Database', icon: Database }
 	];
+  skills = [...skills, ...skills]
 </script>
 
 <div class="bg-neutral relative p-3
   rotate-0 translate-x-0 translate-y-0
-  md:rotate-12 md:translate-x-[25%] md:translate-y-[-10%]
-
-">
-	<div class="flex 
+  md:rotate-12 md:translate-x-[25%] md:translate-y-[-10%]"
+>
+	<div class="flex
     md:grid md:grid-cols-2 gap-3
-    overflow-hidden"
+    animate-slideX md:animate-slideY hover:no-animation"
   >
 		{#each skills as { name, icon }}
 			<div class="min-w-[100px] bg-base-content flex justify-center items-center flex-col rounded-md aspect-square gap-3">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        slideX: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        slideY: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(-50%)' },
+        },
+      },
+      animation: {
+        slideX: 'slideX 30s linear infinite',
+        slideY: 'slideY 100s linear infinite',
+      }
+    }
   },
   plugins: [require("daisyui")],
   daisyui: {
