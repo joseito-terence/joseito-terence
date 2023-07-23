e6b5f16746e8e2394c69f491356152a2b3fd7db5
Joseito Fernandes <joseito.terence@gmail.com>
2023-07-23 11:05:30 +0530

    project cards

---
+++ src/routes/ProjectCard.svelte
@@ -0,0 +1,41 @@
+<script>
+	import IoLogoGithub from 'svelte-icons/io/IoLogoGithub.svelte';
+	import FaFigma from 'svelte-icons/fa/FaFigma.svelte';
+	import IoIosArrowForward from 'svelte-icons/io/IoIosArrowForward.svelte';
+</script>
+
+<div
+	class="bg-secondary-content border-neutral-focus border-2 rounded-xl p-3 md:p-5
+  flex flex-col md:flex-row gap-5"
+>
+	<div class="flex flex-col justify-between">
+		<div>
+			<h1 class="text-2xl tracking-wide font-bold mb-2">Project 1</h1>
+			<p class="text-lg font-light">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente molestias veritatis ut
+				quia nobis totam optio quos commodi suscipit voluptatibus soluta nostrum molestiae
+				consectetur et, maxime, earum pariatur sed. Nisi!
+			</p>
+
+			<div class="text-sm my-3">
+				<span class="rounded bg-neutral-focus py-1 px-2">React</span>
+				<span class="rounded bg-neutral-focus py-1 px-2">Firebase</span>
+				<span class="rounded bg-neutral-focus py-1 px-2">Bootstrap</span>
+				<span class="rounded bg-neutral-focus py-1 px-2">Algolia Search</span>
+			</div>
+		</div>
+
+		<div class="flex flex-row gap-3 mt-6">
+			<button class="btn"> View Site <span class="w-[24px]"><IoIosArrowForward /></span> </button>
+			<button class="btn btn-circle p-1" title='View Code'> <IoLogoGithub /> </button>
+			<button class="btn btn-circle p-2" title='View Design'> <FaFigma /> </button>
+		</div>
+	</div>
+
+	<div class="flex items-center w-full">
+		<img
+			src="https://github.com/joseito-terence/speedwagoan/raw/master/readme_images/394.jpg"
+			alt="project 1"
+		/>
+	</div>
+</div>
--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -1,12 +1,21 @@
+<script>
+	import ProjectCard from "./ProjectCard.svelte";
+
+</script>
 <section id="projects" class="relative overflow-hidden">
 	<div
 		class="text-neutral/40 dark:text-neutral-content/5
     font-extrabold text-[12rem] leading-none tracking-widest uppercase
-    w-[110%] absolute ms-[-3%]"
+    w-[110%] absolute ms-[-3%] select-none"
 	>projects</div>
 
-	<div class="container mt-6">
+	<div class="container my-6">
 		<h1 class="font-bold uppercase tracking-widest text-3xl relative py-9 my-5">Projects</h1>
 
+    <div class="mt-10 flex flex-col gap-4">
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+    </div>
 	</div>
 </section>
