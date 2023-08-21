beaf2a6b7dcabe75056c4d315c4509fcb05784e7
Joseito Fernandes <joseito.terence@gmail.com>
2023-08-21 23:17:46 +0530

    send email animation

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -37,12 +37,15 @@
 	}
 </script>
 
-<footer id="contact" class="bg-base-300 dark:bg-base-200 flex justify-center items-center">
+<footer
+	id="contact"
+	class="bg-base-300 dark:bg-base-200 flex justify-center items-center overflow-hidden"
+>
 	<div
-		class="flex flex-col md:flex-row w-full md:w-[700px] md:m-3 bg-slate-400 dark:bg-neutral-focus md:rounded-xl overflow-hidden"
+		class="flex flex-col md:flex-row w-full md:w-[700px] md:m-3 bg-slate-400 dark:bg-neutral-focus md:rounded-xl"
 	>
 		<div
-			class="w-full md:w-[270px] bg-neutral dark:bg-secondary-content text-white dark:text-inherit md:rounded-e-xl p-5 flex flex-col justify-between"
+			class="w-full md:w-[270px] bg-neutral dark:bg-secondary-content text-white dark:text-inherit md:rounded-xl p-5 flex flex-col justify-between"
 		>
 			<div>
 				<h1 class="text-3xl font-bold">Contact</h1>
@@ -96,17 +99,67 @@
 					<textarea name="message" id="message" class="textarea textarea-bordered" required />
 				</div>
 
-				<button type="submit" class="btn mt-7 float-right group">
+				<button
+					type="submit"
+					class="btn mt-7 float-right group relative {!isSending && 'pr-[40px]'}"
+				>
 					{#if isSending}
+						Sending
 						<span class="loading loading-spinner" />
 					{:else}
-						<div class="w-[25px] h-[25px] transition-all group-hover:rotate-12">
-							<IoIosPaperPlane />
-						</div>
+						Send Message
 					{/if}
-					Send Message
+					<div
+						class="w-[25px] h-[25px]
+							absolute top-[11px] right-[12px]
+							transition-all group-hover:rotate-12 {isSending && 'plane'}"
+					>
+						<IoIosPaperPlane />
+					</div>
 				</button>
 			</form>
 		</div>
 	</div>
 </footer>
+
+<style>
+	.plane {
+		animation: flyaway 1.3s linear;
+		top: -80px;
+		right: -1000px;
+	}
+
+	@keyframes flyaway {
+		0% {
+			transform: rotate(10deg);
+			top: 13px;
+			right: 25px;
+			height: 30px;
+		}
+		5% {
+			transform: rotate(10deg);
+			top: 13px;
+			right: 0px;
+			height: 30px;
+		}
+		20% {
+			transform: rotate(-20deg);
+			top: 13px;
+			right: -130px;
+			height: 45px;
+		}
+		40% {
+			transform: rotate(10deg);
+			top: -40px;
+			right: -280px;
+			opacity: 1;
+		}
+		100% {
+			transform: rotate(60deg);
+			top: -200px;
+			right: -1000px;
+			height: 0;
+			opacity: 0;
+		}
+	}
+</style>
